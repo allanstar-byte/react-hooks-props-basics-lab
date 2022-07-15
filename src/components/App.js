4540a33140212a55540dd5784184aea847866d87
allanstar-byte <allanmutisya263@gmail.com>
2022-07-15 09:57:52 +0300

    props

--- src/components/App.js
+++ src/components/App.js
@@ -8,13 +8,16 @@ import user from "../data/user";
 console.log(user);
 
 function App() {
-  return (
-    <div>
-      <NavBar />
-      <Home />
-      <About />
-    </div>
-  );
+    return ( <
+        div >
+        <
+        NavBar / >
+        <
+        Home / > <
+        About / >
+        <
+        /div>
+    );
 }
 
 export default App;
